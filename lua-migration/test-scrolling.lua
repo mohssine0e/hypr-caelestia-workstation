b7cf1fd879bf2b13b9ae4ec3f39a66235903f552
mohssine <mohssineechlaihi85@gmail.com>
2026-06-06 13:56:14 +0100

    lua: add scrolling migration slice

---
+++ lua-migration/test-scrolling.lua
@@ -0,0 +1,16 @@
+-- Inactive verification entrypoint through the scrolling migration slice.
+hl.monitor({
+    output = "",
+    mode = "preferred",
+    position = "auto",
+    scale = 1,
+})
+
+require("modules.input")
+require("modules.general")
+require("modules.decoration")
+require("modules.animations")
+require("modules.misc")
+require("modules.group")
+require("modules.gestures")
+require("modules.scrolling")
