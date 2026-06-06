14f9cd42509305c1516415fac01ce8ecb7b88611
mohssine <mohssineechlaihi85@gmail.com>
2026-06-06 13:52:06 +0100

    lua: add animations migration slice

---
+++ lua-migration/test-animations.lua
@@ -0,0 +1,12 @@
+-- Inactive verification entrypoint through the animations migration slice.
+hl.monitor({
+    output = "",
+    mode = "preferred",
+    position = "auto",
+    scale = 1,
+})
+
+require("modules.input")
+require("modules.general")
+require("modules.decoration")
+require("modules.animations")
