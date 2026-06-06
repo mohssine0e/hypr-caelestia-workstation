bde3da3509b537285d2bdf82560a0b06d5ca9352
mohssine <mohssineechlaihi85@gmail.com>
2026-06-06 13:55:14 +0100

    lua: add gestures migration slice

---
+++ lua-migration/test-gestures.lua
@@ -0,0 +1,15 @@
+-- Inactive verification entrypoint through the gestures migration slice.
+hl.monitor({
+    output = "",
+    mode = "preferred",
+    position = "auto",
+    scale = 1,
+})
+
+require("modules.input")
+require("modules.general")
+require("modules.decoration")
+require("modules.animations")
+require("modules.misc")
+require("modules.group")
+require("modules.gestures")
