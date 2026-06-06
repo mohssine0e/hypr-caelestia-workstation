088233aefe4d8fcd39504cc6ee9f11a3418f1fe0
mohssine <mohssineechlaihi85@gmail.com>
2026-06-06 13:57:15 +0100

    lua: add env migration slice

---
+++ lua-migration/test-env.lua
@@ -0,0 +1,17 @@
+-- Inactive verification entrypoint through the env migration slice.
+hl.monitor({
+    output = "",
+    mode = "preferred",
+    position = "auto",
+    scale = 1,
+})
+
+require("modules.input")
+require("modules.general")
+require("modules.decoration")
+require("modules.animations")
+require("modules.misc")
+require("modules.group")
+require("modules.gestures")
+require("modules.scrolling")
+require("modules.env")
